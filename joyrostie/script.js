3f3ce222276a765ca047dbf4ad8025dd460c598f
joyrostie <joyrostie@gmail.com>
2017-09-27 22:42:20 +0300

    home_work

--- joyrostie/script.js
+++ joyrostie/script.js
@@ -1,9 +1,11 @@
 function myFunction() {
-    var zn = document.getElementsByTagName("p");
     var div = document.getElementById('one');
     // console.log(zn);
     alert("ВНЕЗАПНО!");
-    div.insertAdjacentHTML("afterEnd", '<img src="https://media2.giphy.com/media/10T5zdNwbdwbVm/200_s.gif">');
+    var img = document.createElement("img");
+    img.src = "https://media2.giphy.com/media/10T5zdNwbdwbVm/200_s.gif"
 
+    div.appendChild(img);
 
+    // div.insertAdjacentHTML("afterEnd", '<img src="https://media2.giphy.com/media/10T5zdNwbdwbVm/200_s.gif">');
 }
