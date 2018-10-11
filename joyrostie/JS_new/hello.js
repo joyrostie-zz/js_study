100014d208ec2b8eecd972a77030bf8056355768
joyrostie <valentyn.dembitskyi@itera.no>
2018-10-11 20:49:10 +0300

    ok

--- joyrostie/JS_new/hello.js
+++ joyrostie/JS_new/hello.js
@@ -1,56 +1,18 @@
+
 ; (function () {
   'use strict';
 
-  // Объявление
-  function PropertyError(property) {
-    this.name = "PropertyError";
+  let group = {
+    title: "Наш курс",
+    students: ["Вася", "Петя", "Даша"],
 
-    this.property = property;
-    this.message = "Ошибка в свойстве " + property;
-
-    if (Error.captureStackTrace) {
-      Error.captureStackTrace(this, PropertyError);
-    } else {
-      this.stack = (new Error()).stack;
+    showList: function () {
+      this.students.forEach((function (student) {
+        alert(this.title + ': ' + student);
+      }));
     }
+  };
 
-  }
-
-  PropertyError.prototype = Object.create(Error.prototype);
-
-  // Генерация ошибки
-  function readUser(data) {
-
-    var user = JSON.parse(data);
-
-    if (!user.age) {
-      throw new PropertyError("age");
-    }
-
-    if (!user.name) {
-      throw new PropertyError("name");
-    }
-
-    return user;
-  }
-
-  // Запуск и try..catch
-
-  try {
-    var user = readUser('{ "age": 25 }');
-  } catch (err) {
-    if (err instanceof PropertyError) {
-      if (err.property == 'name') {
-        // если в данном месте кода возможны анонимы, то всё нормально
-        alert("Здравствуйте, Аноним!");
-      } else {
-        alert(err.message); // Ошибка в свойстве ...
-      }
-    } else if (err instanceof SyntaxError) {
-      alert("Ошибка в синтаксисе данных: " + err.message);
-    } else {
-      throw err; // неизвестная ошибка, не знаю что с ней делать
-    }
-  }
+  group.showList();
 
 })();
